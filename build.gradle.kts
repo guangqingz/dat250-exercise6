c8b6840ec7a27fcaf68bf9ed2dafb41a764e19de
guangqingz <zgq3@hotmail.com>
2025-10-05 01:11:25 +0200

    Added Redis dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.hibernate.orm:hibernate-core:7.1.1.Final")
     implementation("com.h2database:h2:2.3.232")
 	implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
+	implementation("redis.clients:jedis:6.2.0")
 }
 
 tasks.withType<Test> {
